64460ab7af6bf71708f78e2f4d2587289798d165
vainiusd <vainiusd@gmail.com>
2025-12-10 16:02:45 +0200

    remove comments

--- zones/chicfycdn.com/zoneconfig.js
+++ zones/chicfycdn.com/zoneconfig.js
@@ -4,12 +4,6 @@ D("chicfycdn.com", REG_NONE, DnsProvider(DNS_OUTSIDE), DnsProvider(DNS_INSIDE),
   CNAME("www", "@", TTL("1h"))
 );
 
-// D("chicfycdn.com", REG_NONE, DnsProvider(DNS_INSIDE),
-//   A("@", "192.0.2.1"),
-//   AAAA("@", "2001:DB8::1"),
-//   CNAME("www", "@", TTL("1h"))
-// );
-
 D("local.chicfycdn.com", REG_NONE, DnsProvider(DNS_INSIDE),
   A("server", "10.32.0.1"),
 );
